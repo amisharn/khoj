beb8d48a12fa1c20dd23b1bd3cbc36ae79fbe8af
amisharn <niroulaamisharaj@gmail.com>
2026-07-23 20:09:18 +0545

    Complete TF-IDF pipeline in single file

--- backend/app/retrieval/base.py
+++ backend/app/retrieval/base.py
@@ -29,9 +29,13 @@ def inverted_index(preprocessed_text):
 
 
 def preprocess_query(query):
-    query = re.sub(r'[^\w\s]','',query)
-    query = query.lower()
-    return query
+    query_return = []
+    query_terms = query.split()
+    for query in query_terms:
+        query = re.sub(r'[^\w\s]','',query)
+        query = query.lower()
+        query_return.append(query)
+    return query_return
     
 
 
@@ -41,12 +45,16 @@ def search(query,index):
 def term_frequency(preprocessed_docs):
     count = {}
     for doc,text in preprocessed_docs.items():
+        total_words = 0
         count[doc] = {}
         for word in text:
             if word not in count[doc]:
                 count[doc][word] = 1
             else:
                 count[doc][word]+=1
+        total_words = len (text)
+        for word in count[doc]:
+            count[doc][word] = count[doc][word]/total_words
     return count
 
 def document_frequency(count):
@@ -66,14 +74,47 @@ def idf(docs,df):
         idf_count[word] = math.log(N/freq)
     return idf_count
 
+def tf_idf(tf,idf):
+    tf_idf = {}
+    for doc,words in tf.items():
+        tf_idf[doc] = {}
+        for word,occurence in words.items():
+            tf_idf[doc][word] =  occurence * idf [word]
+    return tf_idf
+
+
+def build_vocabulary(tfidf):
+    vocab = set()
+    for doc,values in tfidf.items():
+        for word,value in values.items():
+            vocab.add(word)
+    return sorted(vocab)
+
+def vectorize(tfidf,vocabulary):
+    vector = {}
+    for doc,values in tfidf.items():
+        vector[doc] = []
+        for word in vocabulary:
+            if word in values:
+                vector[doc].append(values[word])
+            else:
+                vector[doc].append(0)
+    return vector
+
+
 count = (term_frequency(preprocess_docs(docs)))
 
 df = document_frequency(count)
 
 idf_val = idf(docs,df)
 
-print(idf_val)
+tfidf = tf_idf(count,idf_val)
 
+vocab = build_vocabulary(tfidf)
+
+print(vocab)
+
+print (vectorize(tfidf,vocab))
 
 
     
